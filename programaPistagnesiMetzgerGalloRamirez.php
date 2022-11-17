dbc076900d2db9179f16a300e05c8ab43fcd8868
GermanMetzger <metzgergerman@gmail.com>
2022-11-17 00:09:17 -0300

    Agregado de las 5 palabreas

--- programaPistagnesiMetzgerGalloRamirez.php
+++ programaPistagnesiMetzgerGalloRamirez.php
@@ -24,8 +24,9 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* ... COMPLETAR ... Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS",
+        "CALLE", "PRIMA", "CARTA", "JUEGO", "PASTA"
+        
     ];
 
     return ($coleccionPalabras);
